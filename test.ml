ab99f9b0dd54fd2309f4152649a3dfe327d85f22
Francois BERENGER <ligand@free.fr>
2017-06-13 17:51:11 +0900

    added test

--- test.ml
+++ test.ml
@@ -1,5 +1,34 @@
 
+let n = 1_000_0000
+let inputs = Array.init n (fun i -> i)
+let counter = ref 0
+
+let demux () =
+  if !counter = n then
+    raise Parany.End_of_input
+  else
+    let res = inputs.(!counter) in
+    incr counter;
+    res
+
+let work a =
+  a
+
+let res_counter = ref 0
+let results = Array.make n 0
+
+let mux x =
+  (* Log.info "Test.mux: %d" x; *)
+  results.(!res_counter) <- x;
+  incr res_counter
+
 let main () =
-  failwith "not implemented yet"
+  (* Log.set_output stdout; *)
+  (* Log.set_log_level Log.DEBUG; *)
+  let nprocs = int_of_string Sys.argv.(1) in
+  Parany.run nprocs demux work mux;
+  for i = 0 to n - 1 do
+    Printf.printf "%d\n" results.(i)
+  done
 
 let () = main ()
