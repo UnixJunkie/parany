78d4537d83340ec0e5ccbd634dd1b5b2e47fe327
Francois BERENGER <ligand@free.fr>
2017-07-03 14:24:17 +0900

    the test exe is OK

--- test.ml
+++ test.ml
@@ -25,14 +25,26 @@ let mux x =
   incr res_counter
 
 let main () =
+  Log.set_log_level Log.INFO;
   let argc = Array.length Sys.argv in
-  if argc <> 2 then
-    (eprintf "usage: %s <int>\n" Sys.argv.(0);
+  if argc <> 3 then
+    (eprintf "usage: %s nprocs csize\n" Sys.argv.(0);
      exit 1);
   let nprocs = int_of_string Sys.argv.(1) in
-  Parany.run ~csize:1 ~nprocs ~demux ~f:work ~mux;
+  let csize = int_of_string Sys.argv.(2) in
+  Parany.run ~csize ~nprocs ~demux ~work ~mux;
   for i = 0 to n - 1 do
     Printf.printf "%d\n" results.(i)
   done
+
+  (* Log.set_log_level Log.INFO; *)
+  (* (\* test we can and write in an mbox *\) *)
+  (* let (input: int list Netcamlbox.camlbox) = Mbox.Readable.create "testmbox" 1 1000 in *)
+  (* let (output: int list Netcamlbox.camlbox_sender) = Mbox.Writable.create "testmbox" in *)
+  (* Mbox.Writable.write output [1;2;3]; *)
+  (* let _ = Mbox.Readable.process_many input (fun l -> *)
+  (*     printf "n: %d\n" (List.length l) *)
+  (*   ) in *)
+  (* () *)
 
 let () = main ()
