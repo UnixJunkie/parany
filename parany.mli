12237ff848114bb825b3bc8b43ba554db2b4e3ab
Francois BERENGER <ligand@free.fr>
2017-07-04 16:39:26 +0900

    even simpler interface for users

--- parany.mli
+++ parany.mli
@@ -1,9 +1,5 @@
 
+(* the demux function must throw End_of_input once it's done *)
 exception End_of_input
 
-val run:
-  ?csize:int ->
-  nprocs:int ->
-  demux:(unit -> 'a) ->
-  work:('a -> 'b) ->
-  mux:('b -> unit) -> unit
+val run: nprocs:int -> demux:(unit -> 'a) -> work:('a -> 'b) -> mux:('b -> unit) -> unit
