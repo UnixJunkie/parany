061db2f5450f1fc93e71787ac1be1429ed84593e
Francois BERENGER <ligand@free.fr>
2017-08-07 10:29:42 +0900

    added ocamldoc in .mli

--- parany.mli
+++ parany.mli
@@ -1,7 +1,19 @@
 
-(* the demux function must throw End_of_input once it's done *)
+(** The [demux] function must throw [Parany.End_of_input] once it is done. *)
 exception End_of_input
 
+(** [run ~csize:10 ~nprocs:16 ~demux:f ~work:g ~mux:h] will run
+    in parallel on 16 cores the [g] function.
+    Inputs to function [g] are produced by function [f]
+    and grouped by 10 (the chunk size).
+    The demux function [f] must throw [Parany.End_of_input]
+    once it is done.
+    Outputs of function [g] are consumed by function [h].
+    A [csize] of one should be the default.
+    The optimal [csize] depends on your computer, the functions
+    you are using and the granularity of your computation.
+    Elements which are fast to process may benefit from
+    a [csize] greater than one. *)
 val run:
   csize:int ->
   nprocs:int ->
