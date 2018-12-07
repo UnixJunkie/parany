bc3c99638690435c27b2aeff75d22977dcd40a4e
Francois Berenger <unixjunkie@sdf.org>
2018-12-07 10:31:38 +0900

    updated build.sh to call dune

--- build.sh
+++ build.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-set -e
-
-# clear old oasis-generated files
-\rm setup.*
-
-oasis setup
-
-ocaml setup.ml -configure --prefix `opam config var prefix`
-ocaml setup.ml -build
-# ocaml setup.ml -uninstall
-# ocamlfind remove parany
-# ocaml setup.ml -install
-
-#./test.native $1 $2
+dune build @install
