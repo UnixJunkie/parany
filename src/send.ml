290852c2d1b1dfde7325491d1ec667b9b1aec44f
Francois Berenger <unixjunkie@sdf.org>
2020-03-19 17:33:13 +0900

    Send: starts to work

--- src/send.ml
+++ src/send.ml
@@ -4,7 +4,7 @@ module A = Array
 open Printf
 
 let unmarshal_from_file fn =
-  let fd = Unix.(openfile fn [O_RDONLY] 0) in
+  let fd = Unix.(openfile fn [O_RDWR] 0) in
   let a =
     Bigarray.array1_of_genarray
       (Unix.map_file fd Bigarray.char Bigarray.c_layout true [|-1|]) in
@@ -13,23 +13,36 @@ let unmarshal_from_file fn =
   Sys.remove fn;
   res
 
-let marshal_to_file v =
-  let tmp_fn = Filename.temp_file "parany_" "" in
-  let fd = Unix.(openfile tmp_fn [O_WRONLY; O_CREAT; O_EXCL] 0o600) in
+let marshal_to_file fn v =
+  let fd = Unix.(openfile fn [O_RDWR; O_CREAT; O_EXCL] 0o600) in
   let s = Marshal.to_string v [Marshal.No_sharing] in
   ignore(Bytearray.mmap_of_string fd s);
   Unix.close fd
 
+let send sock str =
+  Sendmsg.send sock (Bytes.unsafe_of_string str) 0 (String.length str)
+
+let receive sock buff =
+  let count, none = Sendmsg.recv sock buff 0 (Bytes.length buff) in
+  assert(none = None);
+  Bytes.sub_string buff 0 count
+
 let main () =
   let fd_in, fd_out = Unix.(socketpair PF_UNIX SOCK_DGRAM 0) in
   let message_fn = "/tmp/parany_shm_file" in
-  let _message = A.init 10 (fun i -> i) in
-  let sent = Sendmsg.send fd_in (Bytes.unsafe_of_string message_fn) 0 (String.length message_fn) in
+  let message_out = A.init 10 (fun i -> i) in
+  marshal_to_file message_fn message_out;
+  printf "created: %s\n" message_fn;
+  let sent = send fd_in message_fn in
   printf "sent: %d\n" sent;
   Unix.close fd_in;
   let buff = Bytes.create 80 in
-  let received, _none = Sendmsg.recv fd_out buff 0 80 in
-  printf "received: %d\nmsg: %s\n" received (Bytes.unsafe_to_string buff);
-  Unix.close fd_out
+  let message = receive fd_out buff in
+  Unix.close fd_out;
+  printf "received: %s\n" message;
+  let message_in: int array = unmarshal_from_file message in
+  printf "decoded:";
+  A.iter (printf " %d") message_in;
+  printf "\n"
 
 let () = main ()
