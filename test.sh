0393c3774c3af07661c8924f2a2ba15c17a4cf38
Francois Berenger <unixjunkie@sdf.org>
2020-05-15 14:13:34 +0900

    test.sh: test the new preserve CLI parameter

--- test.sh
+++ test.sh
@@ -6,12 +6,16 @@ NPROCS=`getconf _NPROCESSORS_ONLN`
 
 # parallel with csize > 1
 diff <(seq 0 99999) \
-     <(_build/default/src/test.exe $NPROCS 800 2>/dev/null | sort -n)
+     <(_build/default/src/test.exe $NPROCS 800 0 2>/dev/null | sort -n)
 
 # parallel with csize = 1
 diff <(seq 0 99999) \
-     <(_build/default/src/test.exe $NPROCS 1 2>/dev/null | sort -n)
+     <(_build/default/src/test.exe $NPROCS 1 0 2>/dev/null | sort -n)
+
+# parallel with csize = 1 and preserve
+diff <(seq 0 99999) \
+     <(_build/default/src/test.exe $NPROCS 1 1 2>/dev/null)
 
 # sequential
 diff <(seq 0 99999) \
-     <(_build/default/src/test.exe 1 1 2>/dev/null | sort -n)
+     <(_build/default/src/test.exe 1 1 0 2>/dev/null | sort -n)
