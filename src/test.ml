c72f4d6128df1914016ea4e8a208d9a17c430001
Francois Berenger <unixjunkie@sdf.org>
2020-05-15 14:13:16 +0900

    Test: added preserve CLI parameter

--- src/test.ml
+++ src/test.ml
@@ -23,15 +23,23 @@ let mux x =
   results.(!res_counter) <- x;
   incr res_counter
 
+let bool_of_string = function
+  | "0" -> false
+  | "1" -> true
+  | x -> failwith (sprintf "Test.bool_of_string: %s neither 0 nor 1" x)
+
 let main () =
   let argc = Array.length Sys.argv in
-  if argc <> 3 then
-    (eprintf "usage: %s nprocs csize\n" Sys.argv.(0);
+  if argc <> 4 then
+    (eprintf "usage: %s nprocs csize {0|1}\n" Sys.argv.(0);
      exit 1);
   let nprocs = int_of_string Sys.argv.(1) in
   let csize = int_of_string Sys.argv.(2) in
+  let preserve = bool_of_string Sys.argv.(3) in
   (* Parany.set_core_pinning true; *)
-  Parany.run ~verbose:true ~csize ~nprocs ~demux ~work ~mux;
+  (if preserve
+   then Parany.run ~preserve:true ~csize:csize nprocs ~demux ~work ~mux
+   else Parany.run ~csize:csize nprocs ~demux ~work ~mux);
   for i = 0 to n - 1 do
     Printf.printf "%d\n" results.(i)
   done
