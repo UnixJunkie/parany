7b236107e00324cf019c27386cd253ffba30de2f
Francois Berenger <unixjunkie@sdf.org>
2021-09-16 16:42:59 +0900

    Parany: support preserve

--- src/parany.ml
+++ src/parany.ml
@@ -1,6 +1,7 @@
 
 module Dom = Domainslib
 module Chan = Dom.Chan
+module Ht = Hashtbl
 
 exception End_of_input
 
@@ -58,9 +59,44 @@ let muxer_loop nprocs results mux =
   in
   loop ()
 
+(* demux and index items *)
+let idemux (demux: unit -> 'a) =
+  let demux_count = ref 0 in
+  function () ->
+    let res = (!demux_count, demux ()) in
+    incr demux_count;
+    res
+
+(* work, ignoring item index *)
+let iwork (work: 'a -> 'b) ((i, x): int * 'a): int * 'b =
+  (i, work x)
+
+(* mux items in the right order *)
+let imux (mux: 'b -> unit) =
+  let mux_count = ref 0 in
+  (* weak type variable avoidance *)
+  let wait_list = Ht.create 11 in
+  function (i, res) ->
+    if !mux_count = i then
+      begin
+        (* unpile as much as possible *)
+        mux res;
+        incr mux_count;
+        if Ht.length wait_list > 0 then
+          try
+            while true do
+              let next = Ht.find wait_list !mux_count in
+              Ht.remove wait_list !mux_count;
+              mux next;
+              incr mux_count
+            done
+          with Not_found -> () (* no more or index hole *)
+      end
+    else
+      (* put somewhere into the pile *)
+      Ht.add wait_list i res
+
 let run ?(preserve = false) ?(csize = 1) (nprocs: int) ~demux ~work ~mux =
-  if preserve then
-    failwith "Parany.run: preserve not supported";
   if nprocs <= 1 then
     (* no overhead sequential version *)
     try
@@ -70,23 +106,45 @@ let run ?(preserve = false) ?(csize = 1) (nprocs: int) ~demux ~work ~mux =
     with End_of_input -> ()
   else
     begin
-      (* nprocs workers + demuxer thread + muxer thread *)
-      let jobs = Chan.make_bounded (50 * nprocs) in
-      let results = Chan.make_bounded (50 * nprocs) in
-      (* launch the workers *)
-      let workers =
-        Array.init nprocs (fun _rank ->
-            Domain.spawn (fun _ ->
-                worker_loop jobs results work
-              )
+      if preserve then
+        (* nprocs workers + demuxer thread + muxer thread *)
+        let jobs = Chan.make_bounded (50 * nprocs) in
+        let results = Chan.make_bounded (50 * nprocs) in
+        (* launch the workers *)
+        let workers =
+          Array.init nprocs (fun _rank ->
+              Domain.spawn (fun _ ->
+                  worker_loop jobs results (iwork work)
+                )
+            ) in
+        (* launch the demuxer *)
+        let demuxer = Domain.spawn (fun _ ->
+            demuxer_loop nprocs jobs csize (idemux demux)
           ) in
-      (* launch the demuxer *)
-      let demuxer = Domain.spawn (fun _ ->
-          demuxer_loop nprocs jobs csize demux
-        ) in
-      (* use the current thread to collect results (muxer) *)
-      muxer_loop nprocs results mux;
-      (* wait all threads *)
-      Domain.join demuxer;
-      Array.iter Domain.join workers
+        (* use the current thread to collect results (muxer) *)
+        muxer_loop nprocs results (imux mux);
+        (* wait all threads *)
+        Domain.join demuxer;
+        Array.iter Domain.join workers
+      else
+        (* WARNING: serious code duplication below... *)
+        (* nprocs workers + demuxer thread + muxer thread *)
+        let jobs = Chan.make_bounded (50 * nprocs) in
+        let results = Chan.make_bounded (50 * nprocs) in
+        (* launch the workers *)
+        let workers =
+          Array.init nprocs (fun _rank ->
+              Domain.spawn (fun _ ->
+                  worker_loop jobs results work
+                )
+            ) in
+        (* launch the demuxer *)
+        let demuxer = Domain.spawn (fun _ ->
+            demuxer_loop nprocs jobs csize demux
+          ) in
+        (* use the current thread to collect results (muxer) *)
+        muxer_loop nprocs results mux;
+        (* wait all threads *)
+        Domain.join demuxer;
+        Array.iter Domain.join workers
     end
