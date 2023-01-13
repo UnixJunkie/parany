d5c3646238bcefcb32ad77ea326493c2b88750fb
Francois Berenger <unixjunkie@sdf.org>
2023-01-13 17:07:42 +0900

    Parany: imported DLS module

--- src/parany.ml
+++ src/parany.ml
@@ -2,8 +2,32 @@
 module A = Array
 module Chan = Domainslib.Chan
 module Ht = Hashtbl
+module IntMap = Map.Make(Int)
+module Sem = Semaphore.Binary
 
 exception End_of_input
+
+module DLS = struct
+
+  let write_lock = Sem.make true
+
+  (* apparently, null_f is required to avoid the weak type variable problem *)
+  type 'a store = { null_f: (unit -> 'a);
+                    values: ('a IntMap.t) ref }
+
+  let create (null_f: unit -> 'a): 'a store =
+    { null_f; values = ref IntMap.empty }
+
+  let set (s: 'a store) (v: 'a): unit =
+    let my_id = (Domain.self () :> int) in
+    Sem.acquire write_lock; (* <critical_section> *)
+    s.values := IntMap.add my_id v !(s.values);
+    Sem.release write_lock (* </critical_section> *)
+
+  let get s =
+    IntMap.find (Domain.self () :> int) !(s.values)
+
+end
 
 let id2rank =
   DLS.create (fun () -> -1)
